c34c181cfa3778066d5b23a313bc89a82926b98a
SEC-MASUM <masum.jess42@gmail.com>
2023-05-22 01:05:55 +0600

    organize server and db connection code using Clean File Structure

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,15 @@
-import express, { Application } from "express";
-import cors from "cors";
-import "dotenv/config";
-import mongoose from "mongoose";
+import { app } from "./app";
+import { log } from "./app/utils/logger";
+const { PORT } = process.env;
 
-const app: Application = express();
-
-// using cors
-app.use(cors());
-
-//parse data
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-const { URI } = process.env;
-const port = process.env.PORT || 5000;
-console.log(URI);
-
-// connect to server
-const dbConnect = async (): Promise<void> => {
-  if (!URI) {
-    throw new Error("URI is not defined");
+const startServer = async (): Promise<void> => {
+  try {
+    app.listen(PORT, () => {
+      log.info(`Server listening on port ${PORT}`);
+    });
+  } catch (error: any) {
+    log.error(error.message);
   }
-  await mongoose.connect(URI).then(() => console.log("Database Connected"));
-  const db = mongoose.connection;
-
-  //collection name
-  const collection = await db.collection("students");
-
-  app.get("/students", async (req, res) => {
-    const result = await collection.find({}).limit(10).toArray();
-    res.send(result);
-  });
 };
 
-dbConnect();
-
-//route
-app.get("/", (req, res) => {
-  res.send({
-    message: "Our server is ready",
-    status: 200,
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+startServer();
